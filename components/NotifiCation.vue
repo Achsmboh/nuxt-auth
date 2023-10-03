3e661712ac657326daf08f8eb5c74c2e57a9b87e
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-10-03 23:54:17 +0700

    nuxt authentication

---
+++ components/NotifiCation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="notification is-danger">
+    {{ message }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'NotifiCation',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['message'],
+}
+</script>
